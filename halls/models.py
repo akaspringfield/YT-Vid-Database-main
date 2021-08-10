ea33fbef5ece08691cc2b1eebd3677d0473fb15c
Aku <akash.tvm@gmail.com>
2021-08-10 18:55:27 +0530

    creating models

--- halls/models.py
+++ halls/models.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Hall(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
