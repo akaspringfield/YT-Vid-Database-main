31d141a6c4115035c2c25a974d19d98eb8396db4
Aku <akash.tvm@gmail.com>
2021-08-13 10:50:59 +0530

    login view

--- hofvidz/urls.py
+++ hofvidz/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
 
     #Authentication
     path('signup', views.SignUp.as_view(),name='signup'),
-    path('login', auth_views.LoginView.as_view(),name='login'),
+    path('login', auth_views.LoginView.as_view(),name='logins'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
