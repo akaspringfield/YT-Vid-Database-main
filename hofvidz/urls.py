075319abf0f4285f6d2e7f4971c1858adb0fd441
Aku <akash.tvm@gmail.com>
2021-08-14 08:52:53 +0530

    login view

--- hofvidz/urls.py
+++ hofvidz/urls.py
@@ -28,17 +28,6 @@ urlpatterns = [
     #Authentication
     path('signup', views.SignUp.as_view(),name='signup'),
     path('login', auth_views.LoginView.as_view(),name='login'),
-    path('logout', auth_views.LogoutView.as_view(),name='logout'),
-    #hall
-    path('halloffame/create', views.CreateHall.as_view(), name='create_hall'),
-    path('halloffame/<int:pk>', views.DetailHall.as_view(), name='detail_hall'),
-    path('halloffame/<int:pk>/update', views.UpdateHall.as_view(), name='update_hall'),
-    path('halloffame/<int:pk>/delete', views.DeleteHall.as_view(), name='delete_hall'),
-
-    #Video
-    path('halloffame/<int:pk>/addvideo', views.add_video, name='add_video'),
-    path('video/search', views.video_search, name='video_search'),
-    path('video/<int:pk>/delete', views.DeleteVideo.as_view(), name='delete_video'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
