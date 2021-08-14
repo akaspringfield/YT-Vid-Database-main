1ee277a06a1824218997a6f96387cb30f6ae5236
Aku <akash.tvm@gmail.com>
2021-08-14 11:58:27 +0530

    login view

--- halls/views.py
+++ halls/views.py
@@ -3,7 +3,10 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from .models import Hall, Video
-
+from .forms import VideoForm, SearchForm
+from django.contrib.auth import authenticate, login
+from django.http import Http404, JsonResponse
+from django.forms.utils import ErrorList
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 import urllib
