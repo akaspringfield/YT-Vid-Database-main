c0ec78541daf2c2c51286e85d7bc6157cf80c265
Aku <akash.tvm@gmail.com>
2021-09-05 11:05:35 +0530

    making new views

--- hofvidz/urls.py
+++ hofvidz/urls.py
@@ -32,8 +32,6 @@ urlpatterns = [
     #hall
     path('halloffame/create', views.CreateHall.as_view(), name='create_hall'),
     path('halloffame/<int:pk>', views.DetailHall.as_view(), name='detail_hall'),
-    path('halloffame/<int:pk>/update', views.UpdateHall.as_view(), name='update_hall'),
-    path('halloffame/<int:pk>/delete', views.DeleteHall.as_view(), name='delete_hall'),
 
     #Video
     path('halloffame/<int:pk>/addvideo', views.add_video, name='add_video'),
