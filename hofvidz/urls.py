2fde416898010a7f99f286eecac6383879834862
Aku <akash.tvm@gmail.com>
2021-09-05 12:01:52 +0530

    error fixing in url import

--- hofvidz/urls.py
+++ hofvidz/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin, auth
 from django.contrib.auth import views as auth_views
 from django.urls import path
+from halls import views
 from django.conf.urls.static import static
 from django.conf import settings
 
